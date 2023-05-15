a337b26340a18474b44fad87f80c55241f56c3b3
ankbhatia19 <ankbhatia19@gmail.com>
2023-05-14 22:44:11 -0700

    Made some changes to fortune.js such that each method is more modular

--- js/fortune.js
+++ js/fortune.js
@@ -1,32 +1,29 @@
 
-// Initialize the array of fortunes
-let fortunes = [];
-
 /**
  * Load the fortunes from the JSON file
+ * **This function is asynchronous.** It returns a Promise object that will resolve to an array of fortunes.
  *
  * @param {string} url - The URL of the JSON file
- * @returns {boolean} - Returns true if the fortunes were loaded successfully
+ * @returns {Promise<Array>} - An array containing Fortune objects.
  */
 function load(url) {
 
-    fetch(url)                                       // Fetch the JSON file using the fetch function
-        .then(response => response.json()) // Convert the response to a JSON object
+    return fetch(url)                                        // Fetch the JSON file using the fetch function
+        .then(response => response.json())         // Convert the response to a JSON object
         .then(data => {
-            fortunes = data.fortunes;               // Store the array of fortunes in the fortunes variable
-            console.log(data.fortunes);             // Log the array of fortunes to the console
-            return true;                            // Return true to indicate the fortunes were loaded successfully
+            return data.fortunes;                           // Return the list of fortunes
         })
         .catch(error => {
-            console.error(error)                    // Log any errors to the console
-            return false;                           // Return false to indicate the fortunes were not loaded successfully
+            console.error(error)                            // Log any errors to the console
+            return [];
         });
-    return false;
 }
 
 /**
- * Get a random fortune from the array of fortunes. Each fortune has the following properties:
+ * Get a random fortune from the array of fortunes.
+ * Each fortune has the following properties:
  *
+ * ```json
  * {
  *   "title": "Title",
  *   "description": "Sample description",
@@ -40,23 +37,32 @@ function load(url) {
  *      }
  *   }
  * }
+ * ```
+ *
  * Each integer is a value between -100 and 100 inclusive, and reflects the stat changes to the user.
  * For example, a fortune with a rest value of 10 will increase the user's rest by 10.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * @param {Array} array - An array of fortune objects
  * @returns {Object} - A random fortune object
  */
-function roll() {
-    const index = Math.floor(Math.random() * fortunes.length);
-    console.log("Rolled " + index + ": " + fortunes[index]["title"]);
-    return fortunes[index];
+function roll(array) {
+
+    if (array.length === 0) {
+        console.error("Array is empty");
+        return null;
+    }
+
+    const index = Math.floor(Math.random() * array.length);
+    console.log("Rolled " + index + ": " + array[index]["title"]);
+    return array[index];
 }
 
 /**
  * Add two numbers together. This is a simple function to demonstrate unit testing.
  *
- * @param a First number
- * @param b Second number
+ * @param a {int} First number
+ * @param b {int} Second number
  * @returns {int} The sum of the two numbers
  */
 function add(a, b){
@@ -65,17 +71,23 @@ function add(a, b){
 
 window.addEventListener("DOMContentLoaded", () => {
 
+    let fortunes_list = [];
+
     // When the page loads, call the load function
-    load("../json/fortunes.json");
+    load("../json/fortunes.json").then(fortunes => {
+        console.log("Loaded " + fortunes.length + " fortunes");
+        console.log(fortunes);
+        fortunes_list = fortunes;
+    });
 
     // When the button is clicked, call the roll function
     document.querySelector(".fortune-button").addEventListener("click", function(event) {
         event.preventDefault();
         // Roll for the fortune
-        const fortune = roll();
+        const fortune = roll(fortunes_list);
 
-        document.querySelector(".fortune-description").innerHTML = fortune.description;
-        document.querySelector(".fortune-title").innerHTML = fortune.title;
+        document.querySelector(".fortune-description").innerHTML = fortune["description"];
+        document.querySelector(".fortune-title").innerHTML = fortune["title"];
         document.querySelector(".fortune-effects").innerHTML =
             "<br>Effects: <br>" +
             "Rest: " + fortune["effects"]["rest"] + "<br>" +
@@ -88,4 +100,7 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-module.exports = {load, roll, add};
+// Only export the functions if we are running in a Node.js environment (i.e. running tests)
+if (typeof module === 'object') {
+    module.exports = {load, roll, add};
+}
